f4789e5f548a67e87f4860c0db4d1f294e47a167
NadezhdaTrofimova <trofimnad1991@gmail.com>
2021-08-03 16:58:42 +0300

    Done task 2 week 3 day 2

--- week3day2/week3day2.js
+++ week3day2/week3day2.js
@@ -7,6 +7,14 @@ function consoleLogArguments() {
     console.log(arguments[i]);
 }
 
+//Проверка:
 consoleLogArguments("1", "2", "3", "4", "5") // 1 2 3 4 5
 
-// 2.
+// 2. Реализуйте функцию, которая получает на вход два числа и проверяет какое из них больше. Функция должна возвращать 1 если первое число больше второго, -1 если меньше и 0 если числа совпадают.
+
+const CompareNumbers = (number1, number2) => (number1 > number2) ? 1 : (number2 > number1) ? -1 : 0;
+
+//Проверка:
+console.log(CompareNumbers (1, 2)); // -1
+console.log(CompareNumbers (2, 1)); // 1
+console.log(CompareNumbers (10, 10)); // 0
