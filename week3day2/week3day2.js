3f159078f62a3b44e6d0c59852cc6b62c8c49b51
NadezhdaTrofimova <trofimnad1991@gmail.com>
2021-08-03 17:15:19 +0300

    Done task 3 week 3 day 2

--- week3day2/week3day2.js
+++ week3day2/week3day2.js
@@ -18,3 +18,35 @@ const CompareNumbers = (number1, number2) => (number1 > number2) ? 1 : (number2 
 console.log(CompareNumbers (1, 2)); // -1
 console.log(CompareNumbers (2, 1)); // 1
 console.log(CompareNumbers (10, 10)); // 0
+
+/* 3. Используя стрелочные функции запишите следующие функции:
+    а. Функция суммирования двух чисел
+    b. Функция разности двух чисел
+    c. Функция умножения двух чисел
+    d. Функция деления двух чисел   */
+
+// а. Функция суммирования двух чисел:
+const calculateTheSum = (number1, number2) => number1 + number2;
+
+
+// Проверка:
+console.log(calculateTheSum(5,7));  // 12
+
+// b. Функция разности двух чисел:
+const calculateTheResidual = (number1, number2) => number1 - number2;
+
+// Проверка:
+console.log(calculateTheResidual(5,7));  // -2
+
+// c. Функция умножения двух чисел:
+const calculateTheProduct = (number1, number2) => number1 * number2;
+
+// Проверка:
+console.log(calculateTheProduct(5,7));  // 35
+
+// d. Функция деления двух чисел:
+const calculateTheQuotient = (number1, number2) => number1 / number2;
+
+// Проверка:
+console.log(calculateTheQuotient (5,7));  // 0.7142857142857143
+
