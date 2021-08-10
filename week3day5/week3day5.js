3b837d0a1dcab16a5b08a21aa20c623b20d0f368
Nadezhda <trofimnad1991@gmail.com>
2021-08-11 00:38:32 +0300

    done 1 task 1 chapter 5 day 3 week

---
+++ week3day5/week3day5.js
@@ -0,0 +1,9 @@
+// Упражнения. Часть 1.
+
+// 1. Добавьте на страницу элемент div, установите ему класс container. Найдите этот элемент в DOM по классу как посредством getElementsByClassName. так и с помощью querySelector. Повторите тот же процесс но установив другой элемент div с атрибутом id равным main_containter
+
+let element1 = document.getElementsByClassName('.container');
+let element2 = document.querySelector('.container');
+let element3 = document.getElementsByClassName('#main_container');
+let element4 = document.querySelector('#main_container');
+
