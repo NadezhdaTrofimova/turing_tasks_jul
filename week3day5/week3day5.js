fe123c7113648a9897238f0a6476a0f1c8164585
Nadezhda <trofimnad1991@gmail.com>
2021-08-11 01:45:10 +0300

    done 2 task 1 chapter 5 day 3 week

--- week3day5/week3day5.js
+++ week3day5/week3day5.js
@@ -7,3 +7,26 @@ let element2 = document.querySelector('.container');
 let element3 = document.getElementsByClassName('#main_container');
 let element4 = document.querySelector('#main_container');
 
+console.log(element1, element2, element3, element4);
+
+
+// 2. Создать div и стилизовать его динамически в js коде. Для этого:
+// Используйте свойство className
+// Используйте свойство classList
+// Установив id элемента
+
+let div2 = document.createElement('div');
+
+div2.className = 'div-2a';
+div2.classList.add('div-2b');
+div2.id = 'div-2c';
+
+div2.style.width = '200px';
+div2.style.height = '300px';
+div2.style.background = 'red';
+document.body.appendChild(div2);
+
+console.log(div2);
+
+
+
