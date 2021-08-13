ffc1e1da65fd0bd0846e192199dc0a484a9d4bb5
Nadezhda <trofimnad1991@gmail.com>
2021-08-13 15:32:32 +0300

    do 2 task 1 day 4 week

--- week4day1/week4day1.js
+++ week4day1/week4day1.js
@@ -7,12 +7,7 @@ class Employee {
         this.salaryPerHour = salaryPerHour;
         this.hours = hours;
     }
-
-    get getSalary() {
-        return this.calcSalary();
-    }
-
-    calcSalary() {
+    getSalary() {
         return `${this.name} has a salary of ${this.salaryPerHour * this.hours} rub.`;
     }
 }
@@ -20,6 +15,45 @@ class Employee {
 const firstEmployee = new Employee('Gena', 43, 500, 160);
 const secondEmployee = new Employee('Vasya', 27, 400, 183);
 
-console.log(firstEmployee.getSalary) // Gena has a salary of 80000 rub.
-console.log(secondEmployee.getSalary) // Vasya has a salary of 73200 rub.
+console.log(firstEmployee.getSalary()) // Gena has a salary of 80000 rub.
+console.log(secondEmployee.getSalary()) // Vasya has a salary of 73200 rub.
+
+// 2. Опишите все свойства класса из предыдущего упражнения как приватные, а для доступа к ним реализуйте геттеры и сеттеры
+
+class EmployeePrivat {
+    #name;
+    #age;
+    #salaryPerHour;
+    #hours;
+        constructor(name, age, salaryPerHour, hours) {
+            this.#name = name;
+            this.#age = age;
+            this.#salaryPerHour = salaryPerHour;
+            this.#hours = hours;
+    }
+
+    get name() {
+        return this.#name
+    }
+
+    get age() {
+        return this.#age
+    }
+
+    get salaryPerHour() {
+        return this.#salaryPerHour
+    }
+
+    get hours() {
+        return this.#hours
+    }
+
+}
+
+const thirdEmployee = new EmployeePrivat('Galya', 35, 650, 120);
+
+console.log(thirdEmployee.name);  //  Galya
+console.log(thirdEmployee.age);  // 35
+console.log(thirdEmployee.salaryPerHour);  // 650
+console.log(thirdEmployee.hours);  //120
 
