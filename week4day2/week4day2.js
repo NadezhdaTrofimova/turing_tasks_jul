795a2dfedc641e197084b1d8d0a7bf1b5d01201f
Nadezhda <trofimnad1991@gmail.com>
2021-08-17 21:37:37 +0300

    do 3 task 2 day 4 week

--- week4day2/week4day2.js
+++ week4day2/week4day2.js
@@ -20,42 +20,41 @@ function secondsToTheEndOfTheDay() {
     let currentTime = new Date();
     let endDay = new Date(currentTime.getFullYear(), currentTime.getMonth(), currentTime.getDate() + 1);
     let seconds = Math.round((endDay - currentTime) / 1000);
-    setTimeout('secondsToTheEndOfTheDay()', 500)
+    setTimeout('secondsToTheEndOfTheDay()', 1000)
     document.querySelector('.seconds').innerHTML = 'До конца дня осталось: ' + seconds + " секунд";
 }
 
 secondsToTheEndOfTheDay()
 
 // 3. Выведите на странице инпут и кнопку. В инпут вводится число. По нажатию на кнопку запускается таймер с количеством секунд, равным числу, введенному пользователем
-//
-// const timer = document.querySelector('.timer');
-// const newInput = document.createElement('input');
-// const newButton = document.createElement('button');
-// const result = document.createElement('span');
-//
-// timer.appendChild(newInput);
-// timer.appendChild(newButton);
-// timer.appendChild(result);
-//
-// result.style.border = '2px solid black';
-//
-// newButton.innerHTML = "Запустить таймер";
-//
-// newButton.onclick = function () {
-//     let inputValue = newInput.value;
-//     console.log(inputValue)
-//     let startTime = new Date().getTime() + inputValue;
-//     let endTime = new Date();
-//     console.log(startTime);
-//     console.log(endTime);
-//     console.log(diff);
-//     // let diff = new Date() - inputValue -
-//     //
-//     // let hours = Math.floor(inputValue / 3600);
-//     // let minutes = Math.floor((inputValue / 60) - hours * 3600);
-//     // let seconds = inputValue - hours * 3600 - minutes * 60;
-//     // result.textContent = hours + ":" + minutes + ':' + seconds;
-// }
-//
+
+const timer = document.querySelector('.timer');
+const newInput = document.createElement('input');
+const newButton = document.createElement('button');
+const result = document.createElement('div');
+
+timer.append(newInput);
+timer.append(newButton);
+timer.append(result);
+
+result.style.border = '1px solid black';
+result.style.width = '200px';
+result.style.height = '20px';
+result.style.marginTop = '5px';
+newButton.innerHTML = "Запустить таймер";
+
+
+newButton.onclick = () => {
+    let numberInput = Number(newInput.value);
+    const timer = setInterval(function () {
+        console.log(numberInput);
+        result.textContent = numberInput--;
+
+        if (numberInput < 0) {
+            clearInterval(timer);
+        }
+        }, 1000)
+
+}
 
 
